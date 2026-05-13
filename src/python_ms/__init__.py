9c6611ab7731ca734269cac12395250910096a5b
Lari Liuhamo <lari.liuhamo@gmail.com>
2026-05-13 19:59:55 +0300

    feat: Migrate to uv and switch to dynamic versioning

--- src/python_ms/__init__.py
+++ src/python_ms/__init__.py
@@ -1,8 +1,15 @@
 """Python equivalent of the JavaScript ms package."""
 
+import importlib.metadata
 import sys
 
 from python_ms.ms import _ms
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
 
 # Courtesy of the Fuckit package
 sys.modules[__name__] = _ms(__name__, __doc__)
